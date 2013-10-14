17e251a811bc2239a03f3ae5da150551fb25848e
Jonathan Victorino <jonathanvictorino@unknowna8bbcf0725ba.gateway.2wire.net>
2013-10-13 20:14:32 -0700

    Initial Commit

---
+++ Football App/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Football App
+//
+//  Created by Jonathan Victorino on 10/13/13.
+//  Copyright (c) 2013 Jonathan Victorino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
